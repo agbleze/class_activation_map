51237116b2c93be85dbaa55cb917f3f102e1cbdf
linus <lagbleze@st.ug.edu.gh>
2024-01-15 19:26:46 +0100

    Update grad_cam.py

--- grad_cam.py
+++ grad_cam.py
@@ -8,6 +8,10 @@ import torchvision
 
 import pandas as pd
 
-
-
+# TO DO:
+## 1. shows CAM whn image is passed to it
+## 2. Develop UI with option to load model in format .pth
+# 3. Otions to upload a zip or folder of images to be visualize  
+# 4. Visualize image with in clusters and when an image is clicked, its CAM should be shown
+# 5. Option to run 
 
