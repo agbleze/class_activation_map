df8404ce2c736499adda01635e59a120bf0ff866
linus <lagbleze@st.ug.edu.gh>
2024-01-22 23:35:32 +0100

    added nbct

--- cam_implementation.py
+++ cam_implementation.py
@@ -1,4 +1,5 @@
 
+#%%
 import os
 import PIL
 import torch
@@ -12,9 +13,9 @@ import torchvision
 from gradcam.utils import visualize_cam
 from gradcam import GradCAM, GradCAMpp
 
-
+#%%
 img_path = "cat.png"
-pil_img = PIL.Image.open(img_path)
+pil_img = PIL.Image.open(img_path).convert('RGB')
 width, height = pil_img.size
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -36,5 +37,47 @@ model_alexnet = models.alexnet(pretrained=True)
 model_vgg = models.vgg16(pretrained=True)
 model_resnet = models.resnet101(pretrained=True)
 model_densenet = models.densenet161(pretrained=True)
+
+#%%
 model_squeezenet = models.squeezenet1_1(pretrained=True)
 
+
+
+#%%
+loaded_configs = [
+    dict(model_type="alexnet", arch=model_alexnet, layer_name="feacture_11"),
+    dict(model_type="vgg", arch=model_vgg, layer_name="features_29"),
+    dict(model_type='resnet', arch=model_resnet, layer_name='layer4'),
+    dict(model_type='densenet', arch=model_densenet, layer_name='features_norm5'),
+    dict(model_type='squeezenet', arch=model_squeezenet, layer_name='features_12_expand3x3_activation')
+]
+
+
+#%%
+
+for model_config in loaded_configs:
+    model_config["arch"].to(device).eval()
+    
+cams = [[cls.from_config(**model_config) for cls in (GradCAM, GradCAMpp)] for model_config in loaded_configs]
+
+#%%
+images = []
+for gradcam, gradcam_pp in cams:
+    mask, _ = gradcam(normalized_torch_img)
+    heatmap, result = visualize_cam(mask,resized_torch_img)
+    
+    mask_pp = gradcam_pp(normalized_torch_img)
+    heatmap_pp, result_pp =visualize_cam(mask, resized_torch_img)
+
+    images.extend([resized_torch_img.cpu(), result, result_pp])
+    
+grid_image = make_grid(images, nrow=3)
+
+#%%
+
+grid = transforms.ToPILImage()(grid_image)
+grid
+
+
+
+# %%
---
+++ neural_based_decision_tree.py
@@ -0,0 +1,57 @@
+
+#%%
+import nltk
+nltk.download("wordnet")
+
+#%%
+from nbdt.model import SoftNBDT, HardNBDT
+from pytorchcv.models.wrn_cifar import wrn28_10_cifar10
+from torchvision import transforms
+
+from nbdt.utils import DATASET_TO_CLASSES, load_image_from_path, maybe_install_wordnet
+
+import sys
+
+maybe_install_wordnet()
+
+#%%
+def load_model():
+    model = wrn28_10_cifar10()
+    model = HardNBDT(
+        pretrained=True, dataset="CIFAR10", arch="wrn28_10_cifar10",
+        model=model
+    )
+    return model
+
+
+def load_image():
+    assert len(sys.argv) > 1
+    im = load_image_from_path("https://bsmedia.business-standard.com/media-handler.php?mediaPath=https://bsmedia.business-standard.com/_media/bs/img/article/2019-12/23/full/1577083902-3265.jpg&width=1200")
+    transform = transforms.Compose([
+      transforms.Resize(32),
+      transforms.CenterCrop(32),
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    x = transform(im)[None]
+    return x
+
+
+def heirerchy_output(outputs, decisions):
+    _, predicted = outputs.max(1)
+    predicted_class = DATASET_TO_CLASSES['CIFAR10'][predicted[0]]
+    print("Predicted class:", predicted_class,
+          "\n\nHeirerch:",
+          ', '.join(['\n{} ({:.2f}%)'.format(info['name'], info['prob'] * 100) 
+          for info in decisions[0]][1:]))
+    
+def main():
+    model = load_model()
+    x = load_image()
+    outputs, decisions = model.forward_with_decisions(x)
+    heirerchy_output(outputs, decisions)
+    
+if __name__ == "__main__":
+    main()
+
+
