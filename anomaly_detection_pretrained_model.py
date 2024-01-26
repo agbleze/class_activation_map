f73e8cc169b917ca4f4c46228bc6ef630f7b1e19
linus <lagbleze@st.ug.edu.gh>
2024-01-26 23:52:44 +0100

    added fn_det_setting

--- anomaly_detection_pretrained_model.py
+++ anomaly_detection_pretrained_model.py
@@ -18,3 +18,14 @@ def seed_setting(sd):
     torch.manual_seed(sd)
     torch.cuda.manual_seed_all(sd)
 
+def fn_det_setting():
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        
+    if torch.__version__ <= Version("1.7"):
+        torch.fn_det_setting(True)
+    else:
+        torch.use_deterministic_algorithms(True)
+        
+    
